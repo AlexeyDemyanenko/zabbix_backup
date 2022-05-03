29c03ce0505cc6012b0f3b55dac8a14115fbb16c
Alexey Demyanenko <alexeydemyanenko@gmail.com>
2022-05-03 11:57:05 +0300

    tst commit from another user

--- zabbix_backup.sh
+++ zabbix_backup.sh
@@ -2,7 +2,7 @@
 
 params="-v -az --delete"
 
-#use rsync
+# use rsync
 echo "create zabbix folder backup"
 rsync $params	/etc/mysql/mariadb.conf.d/50-server.cnf  /home/nordmin/backup/
 rsync $params /etc/zabbix/ /home/nordmin/backup/etc_backup
@@ -10,22 +10,22 @@ rsync $params /usr/share/zabbix/ /home/nordmin/backup/usr_share_zabbix
 rsync $params /etc/nginx/conf.d/ /home/nordmin/backup/nginx_conf
 
 echo "stop zabbix-server"
-#stop zabbix-server for mysqldump DB
+# stop zabbix-server for mysqldump DB
 systemctl stop zabbix-server
 #sleep - waiting time for stoping
 sleep 3
 echo "make zabbixBD dump"
-#make dump
+# make dump
 mysqldump --defaults-extra-file=/home/nordmin/.mysql/mysqldump.cnf  zabbix > /home/nordmin/backup/zabbix.sql
 
 echo "start zabbix-sever service"
-#starting zabbix-server service
+# starting zabbix-server service
 systemctl start zabbix-server
 sleep 3
 echo "making archive of DB"
-#make archive
+# make archive
 tar -czvf /home/nordmin/backup/zabbix.tar.gz -P /home/nordmin/backup/zabbix.sql
-#rsync -avz /home/nordmin/backup/  nordmin@192.168.3.202:/share/zabbix/
+# rsync -avz /home/nordmin/backup/  nordmin@192.168.3.202:/share/zabbix/
 
 
 
